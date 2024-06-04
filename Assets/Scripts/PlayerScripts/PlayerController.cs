becb98c90ee9320c8cf619edbda462aa5a2b706f
AnnaMigitenko <migitenko@gmail.com>
2024-06-04 17:16:51 +0300

    add background

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -3,9 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+   public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour
 {
    [SerializeField] private float _speedPlayer;
+   [SerializeField] private float _bank;
+
+   [SerializeField] private Boundary _boundary;
+
+   private Rigidbody _rbPlayer;
+
+   private void Start()
+   {
+      _rbPlayer = GetComponent<Rigidbody>();
+   }
 
    private void FixedUpdate()
    {
@@ -14,6 +30,14 @@ public class PlayerController : MonoBehaviour
 
       Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
 
-      GetComponent<Rigidbody>().velocity = movement * _speedPlayer;
+      _rbPlayer.velocity = movement * _speedPlayer;
+      _rbPlayer.rotation = Quaternion.Euler( -90,0, _rbPlayer.velocity.x * -_bank);
+
+     _rbPlayer.position = new Vector3
+      (
+         Math.Clamp(_rbPlayer.position.x, _boundary.xMin, _boundary.xMax),
+         Math.Clamp(_rbPlayer.position.y, _boundary.yMin, _boundary.yMax),
+         0.0f
+      );
    }
 }
